c7d1f0b47bff5f233b197a8c6e9b8c20d39dd026
Ryan McCorvie <ryan@mccorvie.org>
2026-08-01 04:40:39 -0700

    build return arcs on prescribed access hairs

--- Schoenflies/MoiseChapter9.lean
+++ Schoenflies/MoiseChapter9.lean
@@ -27,6 +27,7 @@ import Schoenflies.RefinedCyclicCuts
 import Schoenflies.ControlledTailJoin
 import Schoenflies.PolygonalSubarcs
 import Schoenflies.ControlledCrosscuts
+import Schoenflies.HairReturns
 import Schoenflies.RegionalExtensions
 import ClassificationOfSurfaces.Moise.BrokenLine
 import ClassificationOfSurfaces.Moise.PolygonalSchoenflies
