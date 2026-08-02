49cec3eb038ba80fb866e7f6860be17ab95ad865
Ryan McCorvie <ryan@mccorvie.org>
2026-08-02 03:07:51 -0700

    cover recursive collar boundaries by Moise cells

--- Schoenflies/MoiseChapter9.lean
+++ Schoenflies/MoiseChapter9.lean
@@ -28,6 +28,7 @@ import Schoenflies.SegmentChainWalk
 import Schoenflies.CollarBandSegments
 import Schoenflies.MoiseBandCells
 import Schoenflies.MoiseBandCellBounds
+import Schoenflies.MoiseBandCellTopology
 import Schoenflies.NestedCollarStages
 import Schoenflies.ShrinkingMoiseBandCells
 import Schoenflies.PolyhedralDiskNeighborhoods
